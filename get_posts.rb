2e8c03a114b6e10d78f3c0781619528d7cd281b1
Anatoly Chernow <chertoly@gmail.com>
2018-02-25 22:02:19 +0200

    Do not get stuck when there is no 'Load more' button

--- get_posts.rb
+++ get_posts.rb
@@ -13,22 +13,14 @@ header = article.header
 header.attribute :class #=> "_mainc"
 
 div = article.children[1]
-div.children.size #=> 3
+div.children.size #=> 3 with load more; or 2 -- without
 
-div.children[1].attribute :class #=> "_gwyj6"
-# div at the bottom; contains strange shit with iframe
-
-load_more_button = div.children[2]
-load_more_button.attribute :class #=> "_1cr2e _epyes"
-load_more_button.class #=> Watir::Anchor < Watir::HTMLElement
+if div.children.size == 3
+  load_more_button = div.children[2]
+  load_more_button.attribute :class #=> "_1cr2e _epyes"
+  load_more_button.class #=> Watir::Anchor < Watir::HTMLElement
+end
 # "Load more" button at the bottom
-
-
-image_table = div.children[0]
-image_table.attribute :class #=> "_cmdpi"
-# A table with images. 4 rows; 3 images in each of them.
-image_table.children.size #=> 4(rows)
-  
 
 posts_count, _followers, _following = b.spans(class: '_fd86t').map do |span|
   span.text
@@ -37,13 +29,12 @@ posts_count = posts_count.to_i
 puts "#{username} has #{posts_count} public posts." if ENV['DEBUG']
 
 
-# Only 12 images are shown at the moment.
-images = image_table.divs(class: %w[_mck9w _gvoze _f2mse])
-images.size #=> 12
+images = div.divs class: %w[_mck9w _gvoze _tn0ps]
+images.size
 
 b.send_keys :end
 sleep 1
-load_more_button.click
+load_more_button&.click
 
 puts "Count of currently shown images: #{images.count}." if ENV['DEBUG']
 until images.count == posts_count
